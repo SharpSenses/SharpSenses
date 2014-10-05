71d6d868c32bb9358ec848e4a0ecd11297ab9989
andrecarlucci <andrecarlucci@gmail.com>
2014-10-05 00:11:15 -0300

    backup

--- SharpSenses.RealSense/Camera.cs
+++ SharpSenses.RealSense/Camera.cs
@@ -1,5 +1,14 @@
 ﻿namespace SharpSenses.RealSense {
     public class Camera : ICamera {
-        
+        private PXCMSession _session;
+        private PXCMSenseManager _manager;
+
+        public Camera() {
+            _manager = _session.CreateSenseManager();            
+        }
+
+        public void Start() {
+            
+        }
     }
 }
