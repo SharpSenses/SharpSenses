6e245d857ac0a6309f0524156f4fa821eee13a98
Andre Carlucci <andrecarlucci@gmail.com>
2015-02-09 23:37:02 -0200

    more unit tests

--- SharpSenses.Tests/ItemTests.cs
+++ SharpSenses.Tests/ItemTests.cs
@@ -16,7 +16,35 @@ namespace SharpSenses.Tests {
             Assert.AreEqual("IsVisible", prop);
             Assert.AreSame(face, sender);
         }
-    }
 
-    
+        [Test]
+        public void DefaultNoiseThreshold_zero() {
+            var item = new Item();
+            var count = 0;
+            item.Moved += (sender, args) => {
+                count++;
+            };
+            item.NoiseThreshold = 0;
+            item.Position = new Position();
+            item.Position = new Position {
+                Image = new Point3D(1)
+            };
+            Assert.AreEqual(1, count);
+        }
+
+        [Test]
+        public void DefaultNoiseThreshold_ten() {
+            var item = new Item();
+            var count = 0;
+            item.Moved += (sender, args) => {
+                count++;
+            };
+            item.NoiseThreshold = 10;
+            item.Position = new Position();
+            item.Position = new Position {
+                Image = new Point3D(1)
+            };
+            Assert.AreEqual(0, count);
+        }
+    }
 }
